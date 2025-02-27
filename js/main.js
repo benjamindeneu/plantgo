3dc16269fea5b2991cced7d60e9abc4bef8f1576
benjamindeneu <36848101+benjamindeneu@users.noreply.github.com>
2025-02-27 14:35:32 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -183,13 +183,13 @@ async function displaySpecies(response) {
     const pointsBtn = document.createElement('button');
     pointsBtn.classList.add('points-btn');
     pointsBtn.textContent = `${totalPoints} points`;
-    if (totalPoints >= 500 && totalPoints <= 700) {
+    if (totalPoints >= 500 && totalPoints < 700) {
       pointsBtn.classList.add('common-points'); // Grey
-    } else if (totalPoints > 700 && totalPoints <= 1000) {
+    } else if (totalPoints >= 700 && totalPoints < 1000) {
       pointsBtn.classList.add('rare-points'); // Blue
-    } else if (totalPoints > 1000 && totalPoints <= 1500) {
+    } else if (totalPoints >= 1000 && totalPoints < 1500) {
       pointsBtn.classList.add('epic-points'); // Purple
-    } else if (totalPoints > 1500) {
+    } else if (totalPoints >= 1500) {
       pointsBtn.classList.add('legendary-points'); // Golden
     }
 
